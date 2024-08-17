77ff016f04bfbb4aa460985607dc827f7b18a19d
Gaurav Burjwal <gauravburjwal@gmail.com>
2024-08-17 22:54:32 +0530

    feat: Add smooth scrolling with JS

--- js/script.js
+++ js/script.js
@@ -10,3 +10,33 @@ const headerEl = document.querySelector('.header');
 navBtnEl.addEventListener('click', () => {
   headerEl.classList.toggle('nav-open');
 });
+
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(link => {
+  link.addEventListener('click', evt => {
+    console.log('Link clicked');
+    evt.preventDefault();
+    const href = link.getAttribute('href');
+
+    // Scroll back to top
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+
+    // Scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains('main-nav-link')) {
+      console.log('Link clicked 2');
+      headerEl.classList.toggle('nav-open');
+    }
+  });
+});
